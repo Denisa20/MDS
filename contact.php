63f6f37873dd712cbfce72d119f4d859b76de43c
Denisa <em_denisa@yahoo.com>
2017-06-14 17:44:42 +0300

    Am scos formul din pag de contact

--- contact.php
+++ contact.php
@@ -28,6 +28,7 @@ include ("baza_de_date.php");
 				</tr>
 			</table>
 		<!-- Sfarsit Meniul  -->
+
 			<table cellpadding="0" cellspacing="0" border="0" style="height:30px; background:url(images/bg_top.gif) no-repeat " class="header">
 				<tr>
 					<td><img alt="" src="images/spacer.png" width="39" height="1"></td>
@@ -138,7 +139,7 @@ include ("baza_de_date.php");
 <marquee behavior="scroll" direction="up" scrolldelay="0" align="middle">
    <img src="images/1_p1.jpg" border="0" width="150"><br>
    <img src="images/1_p2.jpg" border="0" width="150"><br>
-   <img src="images/2_p7.jpg" border="0" width="150"><br>
+   <img src="images/1_p3.jpg" border="0" width="150"><br>
 </marquee>
 </div>
 
@@ -166,7 +167,7 @@ include ("baza_de_date.php");
 													<tr>
 														<td><img src="images/q-3.gif" alt=""><br></td>
 														<td style="width:429px">
-	<!--  Aici incepe TITLUL CATEGORIEI --> 													
+	<!--  Aici incepe TITLUL CATEGORIEI --> 									Contact:
 											
 	<!--  Aici se termina TITLUL CATEGORIEI -->											
 														</td>
@@ -178,9 +179,7 @@ include ("baza_de_date.php");
 														<td bgcolor="#BEBEBE"><img alt="" src="images/spacer.png" width="1" height="1"></td>
 														<td><img alt="" src="images/spacer.png" width="18" height="1"></td>
 														<td>
- <!--####################################################################################-->
- <!--##########################  Aici incepe Afisarea produselor  #######################--> 
- <!--####################################################################################-->														
+ 								
 
 															<table cellpadding="0" cellspacing="4" border="0" style="width:448px; height:431px" class="product">
 
@@ -188,39 +187,28 @@ include ("baza_de_date.php");
 																	<td>
 																	
  
- <!--##########################  Aici incepe produsul  #######################--> 
+
  																		<table cellpadding="0" cellspacing="0" border="0" style="width:440px ;" >
-																		
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+	<head>
+		<title>Magazin de Carti </title>
+			<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+			<link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+	
+<body>																		
+Ne puteti contacta oricand la : <br><br>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="ro">
-<center>
-<form style="width:400px; background:#e8e8fe; text-align:center;" name="mailer" method="post"  onsubmit="return Validate()" action="contact_mail.php">
- <h3 id="fc_titlu"><u>Trimiteti mesaj</u></h3>
- Numele dv.: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <input type="text" name="nume" id="nume" size="18" maxlength="40" /><br />
- Adresa dv. de e-mail: <input type="text" name="email" id="email" size="18" maxlength="58" /><br />
- Subiect: &nbsp; &nbsp; &nbsp; <input type="text" name="subiect" id="subiect" size="28" maxlength="70" /><br />
- Scrieti mesajul: (<i>maxim 500 caractere</i>)<br />
-  <textarea name="mesaj" id="mesaj" cols="35" rows="6"></textarea>
-  <input type="hidden" name="anti_spam" value="" />
-  <div id="div_as" style="display:none;">
-  <br />
-   <i>Adaugati urmatorul cod:</i> <b id="codas"> </b><br />
-   Verificare: <input type="text" name="anti_spam1" value="" size="7" maxlength="7" />
-  </div>
-  <input type="submit" value="Trimite" id="submit" />
-</form>
-</center>
-<script type="text/javascript" src="contact.js"> </script>
+	Telefon: 0744444444 <br>
+	Facebook: Raftul de Carti <br>
+	Email: raftul_de_carti@mail.com <br>
 
 
+</body>
 
 </table>
-																		
-
- <!--##########################  Aici se termina PRODUSUL  #######################--> 
-																	
-																		
+																			
 																	</td>
 
 																</tr>
